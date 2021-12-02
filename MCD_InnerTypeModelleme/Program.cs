2d8a016e8990d67b7ad27b090a745027c5f20657
emre gemici <gmcc.emree@gmail.com>
2021-12-02 16:21:22 +0300

    code changed

--- MCD_InnerTypeModelleme/Program.cs
+++ MCD_InnerTypeModelleme/Program.cs
@@ -10,7 +10,43 @@ namespace MCD_InnerTypeModelleme
     {
         static void Main(string[] args)
         {
+            Musteri M1 = new Musteri();
 
+            M1.musteriID = 1;
+            M1.tcKimlikNumarasi = "12254401285";
+            M1.isim = "Emre";
+            M1.soyisim = "Gemici";
+            M1.olusturmaTarih = DateTime.Now;
+
+            // M1.musteriAdresleri = new MusteriAdres[5];  Asla yapmayın...
+
+            M1.musteriAdresleri[0] = new MusteriAdres()
+            {
+                Il = "İstanbul",
+                Ilce = "Kadıköy",
+                Adres = "Adres",
+                adresTip = "İş Yeri"
+            };
+
+            M1.musteriAdresleri[0].MusteriAdresTestMetot();
+            M1.musteriSiparisBilgileri[0] = new MusteriSiparisBilgisi()
+            {
+                siparisNumarasi = "SIP0001"
+            };
+
+            M1.musteriSiparisBilgileri[0].urunler[0] = new Urun()
+            {
+                urunId = 1,
+                Tanim = "Telefon",
+                fiyat = 1500
+            };
+
+            M1.musteriSiparisBilgileri[0].urunler[1] = new Urun()
+            {
+                urunId = 2,
+                Tanim = "Tablet",
+                fiyat = 2500
+            };
 
 
             Console.ReadKey();
